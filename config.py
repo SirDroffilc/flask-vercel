45ea4242cfa8e37f008d349eabe5f67e1602611e
Ford Torion <clifford.torion04@gmail.com>
2024-11-16 00:32:22 +0800

    second commit

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres_url_here')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgresqlFord@localhost:5432/flask_vercel'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
