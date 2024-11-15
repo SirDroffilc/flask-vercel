c543c31f28ca8dcafa91d1af7f1589249047192f
Ford Torion <clifford.torion04@gmail.com>
2024-11-16 00:54:46 +0800

    modify

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres_url_here')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres.gnrhccivkfbfxmwxlxcj:Dk6vCJdNYuinpouP@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
